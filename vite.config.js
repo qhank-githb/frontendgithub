1a32ba75807f9cf4e074316cd8c524a6f470f2d7
boloq <q17727969236@qq.com>
2025-08-08 16:12:11 +0800

    更新前端监听端口，从localhost改到0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  server: {
+    host: "0.0.0.0", // 监听所有网卡地址，允许局域网访问
+    port: 3000, // 你想要的端口，可以修改
+  },
+
+  plugins: [vue()],
+});
