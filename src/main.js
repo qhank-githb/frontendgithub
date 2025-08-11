7e93d4381173f12727aaa448bc0d2c7af27e9424
boloq <q17727969236@qq.com>
2025-08-11 14:07:18 +0800

    Element plus包更改为中文

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import zhCn from "element-plus/es/locale/lang/zh-cn";
 
-const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.mount("#app");
